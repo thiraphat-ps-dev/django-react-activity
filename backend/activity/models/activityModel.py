fa6627de1f03a0f0dcc022528e5e573d84a5a232
gun <thiraphat.ps.dev@gmail.com>
2020-06-11 19:07:53 +0700

    Clean activityModel

--- backend/activity/models/activityModel.py
+++ backend/activity/models/activityModel.py
@@ -1,8 +1,6 @@
 
 from django.db import models
-# Create your models here.
 
-# add this
 class Activity(models.Model):
     title = models.CharField(max_length=120)
     description = models.TextField()
