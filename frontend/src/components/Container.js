7ef7402cd68425c551d4bb682866bcc77f0fc1f3
gun <thiraphat.ps.dev@gmail.com>
2020-06-13 10:34:48 +0700

    Create Container component and make scss

---
+++ frontend/src/components/Container.js
@@ -0,0 +1,117 @@
+import React, { Component } from 'react';
+import './../styles/main.scss';
+import Card from './Card';
+import axios from 'axios';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlusSquare, faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons';
+import Modal from './Modal';
+
+export default class Container extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      activityList: [],
+      page: [],
+      modal: false,
+    };
+  }
+  componentDidMount() {
+    this.refreshList();
+  }
+  refreshList = () => {
+    axios
+      .get('http://localhost:8000/api/activitys/')
+      .then((res) => {
+        const pages = [];
+        for (let i = 0; i < parseInt(res.data.page); i++) {
+          pages.push(i + 1);
+        }
+        console.log('psss', pages);
+        this.setState({ activityList: res.data, page: pages });
+      })
+      .catch((err) => console.log(err));
+  };
+
+  openModal() {
+    let card = document.querySelector('.activity-container');
+    let modal = document.querySelector('.modal-container');
+    let c = card ? card.classList.add('modal-open') : null;
+    let m = modal ? modal.classList.add('-open') : null;
+  }
+  render() {
+    console.log(this.state.activityList.results);
+
+    return (
+      <div>
+        <div className="activity-container">
+          <div className="header-text">
+            <div
+              className="btn-add"
+              onClick={() => {
+                this.openModal();
+              }}
+            >
+              <FontAwesomeIcon icon={faPlusSquare} />
+              ADD
+            </div>
+
+            <h1>Activity List</h1>
+          </div>
+          <div className="card">
+            {this.state.activityList.results
+              ? this.state.activityList.results.map((activity, i) => (
+                  <Card key={i} index={i} title={activity.title} completed={activity.completed} start={activity.start_date} end={activity.end_date} />
+                ))
+              : null}
+          </div>
+
+          <div className="footer">
+            <div
+              className="previous"
+              onClick={() => {
+                axios
+                  .get(this.state.activityList.links.previous)
+                  .then((res) => {
+                    const pages = [];
+                    for (let i = 0; i < parseInt(res.data.page); i++) {
+                      pages.push(i + 1);
+                    }
+                    console.log('psss', pages);
+                    this.setState({ activityList: res.data, page: pages });
+                  })
+                  .catch((err) => console.log(err));
+              }}
+            >
+              <FontAwesomeIcon icon={faChevronLeft} />
+            </div>
+            {this.state.activityList.results ? (
+              <div className="page">
+                {this.state.activityList.page} / {Math.ceil(this.state.activityList.total / 5)}
+              </div>
+            ) : null}
+            <div
+              className="next"
+              onClick={() => {
+                axios
+                  .get(this.state.activityList.links.next)
+                  .then((res) => {
+                    const pages = [];
+                    for (let i = 0; i < parseInt(res.data.page); i++) {
+                      pages.push(i + 1);
+                    }
+                    console.log('psss', pages);
+                    this.setState({ activityList: res.data, page: pages });
+                  })
+                  .catch((err) => console.log(err));
+              }}
+            >
+              <FontAwesomeIcon icon={faChevronRight} />
+            </div>
+          </div>
+        </div>
+
+        <Modal />
+      </div>
+    );
+  }
+}
